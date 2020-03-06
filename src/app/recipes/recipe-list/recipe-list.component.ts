78f8b17dd8892c5706e9bc5a91b5d793e0e2fa29
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-06 17:20:19 +0100

    working on recipes: recipes of array are displayed in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'my test recipe',
       'this is the description of the recipe',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'shrimp recipe',
+      'this is a tasty shrimp salad recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
     )
   ];
 
