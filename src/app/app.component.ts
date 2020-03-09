64bcf6693074cc8cf433c2fdefcbf35d11d3b82b
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-09 20:00:47 +0100

    added navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-complete-guide';
+  feature = 'recipe';
+
+  onNavigate(selectedFeature: string) {
+    this.feature = selectedFeature;
+  }
 }
