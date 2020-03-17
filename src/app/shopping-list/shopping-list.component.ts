da69b9a7dfe1659fe18af48ee1aa673c249574f8
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-17 16:24:09 +0100

    replace EventEmitter by Subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
+  ngChangedSub: Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientChanged.subscribe(
+    this.ngChangedSub = this.slService.ingredientChanged.subscribe(
       (ingredients: Ingredient[]) => (this.ingredients = ingredients)
     );
+  }
+
+  ngOnDestroy() {
+    this.ngChangedSub.unsubscribe();
   }
 }
