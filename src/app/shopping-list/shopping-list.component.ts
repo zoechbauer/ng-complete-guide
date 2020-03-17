f395f4cdcbd41a71103c96af47ea1cbd9e6371b7
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-17 16:39:21 +0100

    better wording

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,18 +10,18 @@ import { Subscription } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
-  ngChangedSub: Subscription;
+  subscription: Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.ngChangedSub = this.slService.ingredientChanged.subscribe(
+    this.subscription = this.slService.ingredientChanged.subscribe(
       (ingredients: Ingredient[]) => (this.ingredients = ingredients)
     );
   }
 
   ngOnDestroy() {
-    this.ngChangedSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
