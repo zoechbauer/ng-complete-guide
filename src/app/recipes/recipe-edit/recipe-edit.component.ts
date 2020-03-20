32d79c5263e66dc4731bb029d994d8def7b45ffd
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-20 18:45:08 +0100

    Reactive Form: adding ingredient controls to a form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl, NgForm } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Form } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -12,6 +12,11 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+
+  get controls() {
+    // a getter!
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
 
   constructor(
     private route: ActivatedRoute,
@@ -31,21 +36,38 @@ export class RecipeEditComponent implements OnInit {
     let recipeImgPath = '';
     let recipeDescription = '';
     const recipe = this.recipeService.getRecipe(this.id);
+    console.log(recipe);
+    const ingredients = new FormArray([]);
 
     if (this.editMode) {
       recipeName = recipe.name;
       recipeImgPath = recipe.imagePath;
       recipeDescription = recipe.description;
+
+      if (recipe['ingredients']) {
+        for (const ingredient of recipe.ingredients) {
+          ingredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
     // empty values for new recipe, recipe values for edit recipe
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImgPath),
-      description: new FormControl(recipeDescription)
+      description: new FormControl(recipeDescription),
+      ingredients: new FormArray(ingredients.controls)
     });
   }
 
   onSubmit() {
     console.log(this.recipeForm);
+
+    // console.log(this.controls);
+    // console.log(this.recipeForm.get('ingredients') as FormArray);
   }
 }
