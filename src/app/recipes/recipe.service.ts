03eb969fb446558b58384f0ac51d9504819e5d72
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-25 11:33:03 +0100

    http: hard coded sample values for ingredients and shopping list removed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,20 +6,21 @@ import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Schnitzel',
-      'one of my favorite meals',
-      'https://cdn.pixabay.com/photo/2014/05/28/12/20/wiener-schnitzel-356436_960_720.jpg',
-      [new Ingredient('meat', 1), new Ingredient('French fries', 20)]
-    ),
-    new Recipe(
-      'shrimp recipe',
-      'this is a tasty shrimp salad recipe',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
-      [new Ingredient('shrimps', 5), new Ingredient('salade', 10)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Schnitzel',
+  //     'one of my favorite meals',
+  //     'https://cdn.pixabay.com/photo/2014/05/28/12/20/wiener-schnitzel-356436_960_720.jpg',
+  //     [new Ingredient('meat', 1), new Ingredient('French fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'shrimp recipe',
+  //     'this is a tasty shrimp salad recipe',
+  //     'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+  //     [new Ingredient('shrimps', 5), new Ingredient('salade', 10)]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   recipeChanged = new Subject<Recipe[]>();
 
