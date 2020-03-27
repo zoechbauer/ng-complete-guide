c25df5921fdb4fce4a9632f5245ae32353ae4238
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-27 10:05:53 +0100

    Auth: Refactoring - code enhancements for catchError in login & signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { throwError, Observable } from 'rxjs';
 
 export interface AuthResponseData {
   idToken: string;
@@ -29,20 +29,7 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       })
-      .pipe(
-        catchError(errorRes => {
-          let errorMessage = 'An unknown error occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already!';
-              break;
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
@@ -54,23 +41,28 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       })
-      .pipe(
-        catchError(errorRes => {
-          console.log(errorRes);
-          let errorMessage = 'An unknown error occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_NOT_FOUND':
-              errorMessage = 'The email does not exist!';
-              break;
-            case 'INVALID_PASSWORD':
-              errorMessage = 'The password is not correct!';
-              break;
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    console.log(errorRes);
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      // error messages for login
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'The email does not exist!';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is not correct!';
+        break;
+      // error messages for signup
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already!';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
