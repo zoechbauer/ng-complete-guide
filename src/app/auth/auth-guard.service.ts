4cbbe585b2fdaf8dd54ee79d84edfea253933b7d
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-28 10:42:07 +0100

    Auth: Auth-Guard with redirecting (for elder Angular versions)

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -2,19 +2,27 @@ import { Injectable } from '@angular/core';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot
+  RouterStateSnapshot,
+  Router
 } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return this.authService.user.pipe(map(user => !!user));
+    return this.authService.user.pipe(
+      map(user => !!user),
+      tap(isAuth => {
+        if (!isAuth) {
+          this.router.navigate(['/auth']);
+        }
+      })
+    );
   }
 }
