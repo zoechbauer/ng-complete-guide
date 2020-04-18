2a00236e9d72e36e2ea8213e4977ee41137fbb7c
Zoechbauer <hans.zoechbauer@aon.at>
2020-04-18 16:33:29 +0200

    BugFix: recipes.module.ts location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder.directive';
-import { RecipesModule } from './recipes/recipes/recipes.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @NgModule({
   declarations: [
