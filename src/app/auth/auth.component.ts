60a412820c219016f38bd28d6e3f0a1adf4c65c7
Zoechbauer <hans.zoechbauer@aon.at>
2020-04-23 19:23:27 +0200

    Signup #4: Dispatch SignupStart

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -59,13 +59,12 @@ export class AuthComponent implements OnInit, OnDestroy {
     const email = form.value.email;
     const password = form.value.password;
 
-    let authObs: Observable<AuthResponseData>;
+    // let authObs: Observable<AuthResponseData>;
 
     this.error = null;
     this.isLoading = true;
     if (this.isLoginMode) {
       // authObs = this.authService.login(email, password);
-      // TODO resolve problems for spinner and navigation because of comment out authObs observable
       this.store.dispatch(
         new AuthActions.LoginStart({
           email: email,
@@ -73,7 +72,10 @@ export class AuthComponent implements OnInit, OnDestroy {
         })
       );
     } else {
-      authObs = this.authService.signup(email, password);
+      // authObs = this.authService.signup(email, password);
+      this.store.dispatch(
+        new AuthActions.SignupStart({ email: email, password: password })
+      );
     }
 
     // authObs.subscribe(
