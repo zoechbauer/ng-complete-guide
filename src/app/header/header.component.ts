0acc7c7c377e18062e56f08ccc8e0e2636d7c92a
Zoechbauer <hans.zoechbauer@aon.at>
2020-04-24 10:27:00 +0200

    Logout #2: Dispatch moved from service to component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import { Store } from '@ngrx/store';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
 import * as fromApp from '../store/app.reducer';
+import * as AuthActions from '../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -40,7 +41,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 
   ngOnDestroy() {
