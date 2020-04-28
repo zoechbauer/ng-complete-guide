b285905e108348d89ad7b1a846232aea76a5778c
Zoechbauer <hans.zoechbauer@aon.at>
2020-04-28 10:52:17 +0200

    Angular Universal: Installation & Set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules, initialNavigation: 'enabled' }),
   ],
   exports: [RouterModule],
 })
